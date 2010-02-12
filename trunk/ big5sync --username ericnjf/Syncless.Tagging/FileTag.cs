37eee8cd1a793905ed7171365fa17bc6f076fdba
ericnjf@gmail.com <ericnjf@gmail.com@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-02-12 03:15:03 +0000

    First commit.

---
+++ trunk/ big5sync --username ericnjf/Syncless.Tagging/FileTag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Syncless.Tagging
+{
+    public class FileTag:Tag
+    {
+        public FileTag(string tagname)
+            : base(tagname)
+        {
+
+        }
+        public List<TaggedPath> FilePaths
+        {
+            get { return _pathList; }
+        }
+
+        public override Boolean AddPath(string path)
+        {
+            return false;
+        }
+        public override bool AddPath(string path, string date)
+        {
+            return false;
+        }
+    }
+}
---
+++ trunk/ big5sync --username ericnjf/Syncless.Tagging/Tag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Syncless.Tagging
+{
+    public abstract class Tag
+    {
+        private string _tagName;
+        public string TagName
+        {
+            get { return _tagName; }
+            set { this._tagName = value; }
+        }
+
+        protected List<TaggedPath> _pathList;
+
+        public Tag(string tagname)
+        {
+            TagName = tagname;
+            this._pathList = new List<TaggedPath>();
+        }
+
+        public abstract Boolean AddPath(string path);
+        public abstract Boolean AddPath(string path, string date);
+        public bool Contain(string path)
+        {
+            return false;
+        }
+    }
+}
