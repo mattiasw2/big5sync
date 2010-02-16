0ff6bf5421458dcf955eac446735d92bad82a6c0
ericnjf@gmail.com <ericnjf@gmail.com@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-02-16 08:57:21 +0000

    Update Error Message With Range of Error Code and Region =)

--- trunk/ big5sync/Syncless.Helper/ErrorMessage.cs
+++ trunk/ big5sync/Syncless.Helper/ErrorMessage.cs
@@ -7,6 +7,17 @@ namespace Syncless.Helper
 {
     public static class ErrorMessage
     {
-        
+        #region Tagging Error Messages (Error Code : 100~199)
+
+        #endregion
+        #region Profiling Error Messages (Error Code : 200~299)
+
+        #endregion
+        #region CompareAndSync Error Messages (Error Code : 300~499)
+
+        #endregion
+        #region Monitor Error Messages (Error Code : 500~599)
+
+        #endregion
     }
 }
