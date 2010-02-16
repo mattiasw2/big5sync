e2f1248b180e8e732bc8eae7478c0b198ae86e0d
ericnjf@gmail.com <ericnjf@gmail.com@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-02-16 08:35:37 +0000

    Majority of Syncless.Profiling is done. Need to merge Profile and Convert And filter.

---
+++ trunk/ big5sync/Syncless.Profiling/ProfileConflictException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Syncless.Profiling
+{
+    public class ProfileConflictException : Exception
+    {
+        public ProfileConflictException(string message):base(message)
+        {
+
+        }
+    }
+}
