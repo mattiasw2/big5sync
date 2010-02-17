671f7ac74180f7430bf5428528bab798af4fd123
ericnjf <ericnjf@gmail.com>
2010-02-17 03:32:58 +0000

    committed uncompilable SystemLogicLayer

---  big5sync/Syncless.Core/SystemLogicLayer.cs
+++  big5sync/Syncless.Core/SystemLogicLayer.cs
@@ -32,137 +32,137 @@ namespace Syncless.Core
 
         public List<Tag> GetAllTags()
         {
-            
+            return null;    
         }
 
         public List<Tag> GetAllTags(FileInfo file)
         {
-            
+            return null;
         }
 
         public List<Tag> GetAllTags(DirectoryInfo info)
         {
-            
+            return null;
         }
 
         public FileTag CreateFileTag(string tagname)
         {
-            
+            return null;
         }
 
         public FolderTag CreateFolderTag(string tagname)
         {
-            
+            return null;
         }
 
         public FileTag TagFile(string tagname, FileInfo file)
         {
-            
+            return null;
         }
 
         public FileTag TagFile(FileTag tag, FileInfo file)
         {
-            
+            return null;
         }
 
         public FolderTag TagFolder(string tagname, DirectoryInfo folder)
         {
-            
+            return null;
         }
 
         public FolderTag TagFolder(FolderTag tag, DirectoryInfo file)
         {
-            
+            return null;
         }
 
         public FileTag UntagFile(FileTag tag, FileInfo file)
         {
-            
+            return null;
         }
 
         public FolderTag UntagFolder(FolderTag tag, DirectoryInfo folder)
         {
-            
+            return null;
         }
 
         public bool DeleteTag(FolderTag tag)
         {
-            
+            return false;
         }
 
         public bool DeleteTag(FileTag tag)
         {
-            
+            return false;
         }
 
         public bool DeleteAllTags()
         {
-            
+            return false;
         }
 
         public bool DeleteAllTags(FileInfo file)
         {
-            
+            return false;
         }
 
         public bool DeleteAllTags(DirectoryInfo folder)
         {
-            
+            return false;
         }
 
         public bool StartManualSync(FileTag tagname)
         {
-            
+            return false;
         }
 
         public bool StartManualSync(FolderTag tagname)
         {
-            
+            return false;
         }
 
         public bool MonitorTag(FileTag tag, bool mode)
         {
-            
+            return false;
         }
 
         public bool SetTagBidirectional(FileTag tag)
         {
-            
+            return false;
         }
 
         public bool SetTagBidirectional(FolderTag tag)
         {
-            
+            return false;
         }
 
         public CompareResult PreviewSync(FolderTag tag)
         {
-            
+            return null;
         }
 
         public CompareResult PreviewSync(FileTag tag)
         {
-            
+            return null;
         }
 
         public bool PrepareForTermination()
         {
-            
+            return true;
         }
 
         public bool Terminate()
         {
-            
+            return false;
         }
 
         public bool Initiate()
         {
-            
+            return false;
         }
 
         public Tag TagPath(string tagname, string path)
         {
-            
+            return null;
         }
 
         #endregion
