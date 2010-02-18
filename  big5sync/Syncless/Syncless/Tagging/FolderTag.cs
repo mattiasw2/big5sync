c25d840598552801c6f6838b51a2b9560c779a6f
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-02-18 09:10:17 +0000

    Modified FindMatchedParentDirectory method

---  big5sync/Syncless/Syncless/Tagging/FolderTag.cs
+++  big5sync/Syncless/Syncless/Tagging/FolderTag.cs
@@ -15,40 +15,49 @@ namespace Syncless.Tagging
 
         public string FindMatchedParentDirectory(string path)
         {
-            string[] pathTokens = path.Split('\\');
-            int count1 = pathTokens.Length;
-            if (pathTokens[pathTokens.Length-1].Equals(""))
-            {
-                count1--;
-            }
+            string[] tempPathTokens = path.Split('\\');
+            string[] pathTokens = TrimEnd(tempPathTokens);
             string logicalid = pathTokens[0].TrimEnd(':');
             string trailingPath = null;
             foreach (TaggedPath p in _pathList)
             {
                 if (p.LogicalDriveId.Equals(logicalid))
                 {
-                    string[] pTokens = p.Path.Split('\\');
-                    int count2 = pTokens.Length;
-                    if (pTokens[pTokens.Length - 1].Equals(""))
+                    string[] tempPTokens = p.Path.Split('\\');
+                    string[] pTokens = TrimEnd(tempPTokens);
+                    if (pathTokens.Length > pTokens.Length)
                     {
-                        count2--;
-                    }
-                    if (count1 > count2)
-                    {
-                        int trailingIndex = Match(pathTokens, pTokens);
-                        if (trailingIndex > 0)
+                        if (CheckMatch(pathTokens, pTokens))
                         {
-                            for (int i = trailingIndex; i < pathTokens.Length - 1; i++)
+                            int trailingIndex = Match(pathTokens, pTokens);
+                            if (trailingIndex > 0)
                             {
-                                trailingPath += (pathTokens[i] + "\\");
+                                for (int i = trailingIndex; i < pathTokens.Length - 1; i++)
+                                {
+                                    trailingPath += (pathTokens[i] + "\\");
+                                }
+                                trailingPath += pathTokens[pathTokens.Length - 1];
+                                return trailingPath;
                             }
-                            trailingPath += pathTokens[pathTokens.Length - 1];
-                            return trailingPath;
                         }
                     }
                 }
             }
             return null;
+        }
+
+        private string[] TrimEnd(string[] tempPathTokens)
+        {
+            string[] pathTokens = new string[tempPathTokens.Length-1];
+            if (tempPathTokens[tempPathTokens.Length - 1].Equals(""))
+            {
+                for (int i = 0; i < pathTokens.Length; i++)
+                {
+                    pathTokens[i] = tempPathTokens[i];
+                }
+                return pathTokens;
+            }
+            return tempPathTokens;
         }
 
         private int Match(string[] pathTokens, string[] pTokens)
@@ -66,6 +75,20 @@ namespace Syncless.Tagging
                 }
             }
             return trailingIndex;
+        }
+
+        private bool CheckMatch(string[] pathTokens, string[] pTokens)
+        {
+            bool allMatched = true;
+            for (int i = 0; i < pTokens.Length; i++)
+            {
+                if (!pTokens[i].Equals(pathTokens[i]))
+                {
+                    allMatched = false;
+                    break;
+                }
+            }
+            return allMatched;
         }
     }
 }
