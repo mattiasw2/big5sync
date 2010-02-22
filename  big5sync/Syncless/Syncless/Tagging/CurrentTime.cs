4a03ee4e8102cf75c502201534d4aa82fa0843b7
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-02-22 16:31:38 +0000

    Added CurrentTime and LogMessage classes to Tagging.

---
+++  big5sync/Syncless/Syncless/Tagging/CurrentTime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Syncless.Tagging
+{
+    public class CurrentTime
+    {
+        private static DateTime _currentDateTime;
+
+        public long CurrentTimeLong
+        {
+            get { return GetCurrentTime(); }
+        }
+
+        public string CurrentTimeString
+        {
+            get { return GetCurrentTimeString(); }
+        }
+
+        public CurrentTime()
+        {
+            _currentDateTime = DateTime.Now;
+        }
+
+        private static long GetCurrentTime()
+        {
+            return _currentDateTime.Ticks;
+        }
+
+        private static string GetCurrentTimeString()
+        {
+            string day = (_currentDateTime.Day < 10) ? ("0" + _currentDateTime.Day.ToString()) : _currentDateTime.Day.ToString();
+            string month = (_currentDateTime.Month < 10) ? ("0" + _currentDateTime.Month.ToString()) : _currentDateTime.Month.ToString();
+            string year = _currentDateTime.Year.ToString();
+            string hour = (_currentDateTime.Hour < 10) ? ("0" + _currentDateTime.Hour.ToString()) : _currentDateTime.Hour.ToString();
+            string minute = (_currentDateTime.Minute < 10) ? ("0" + _currentDateTime.Minute.ToString()) : _currentDateTime.Minute.ToString();
+            string second = (_currentDateTime.Second < 10) ? ("0" + _currentDateTime.Second.ToString()) : _currentDateTime.Second.ToString();
+            string datestring = string.Format("{0}/{1}/{2} {3}:{4}:{5}", day, month, year, hour, minute, second);
+            return datestring;
+        }
+    }
+}
