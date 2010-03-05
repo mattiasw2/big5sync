ebdd7eaf929a23d454baed6a739d0a91d518019e
ericnjf <ericnjf@gmail.com>
2010-03-05 12:57:07 +0000

    changes made to FileSizeFilter and AbstractFileFilter

---  big5sync/Syncless/Syncless/Filters/AbstractFileFilter.cs
+++  big5sync/Syncless/Syncless/Filters/AbstractFileFilter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.IO;
 namespace Syncless.Filters
 {
     public abstract class AbstractFileFilter:AbstractFilter
@@ -10,6 +10,22 @@ namespace Syncless.Filters
         public AbstractFileFilter(Mode mode)
             : base(mode)
         {
+        }
+
+        public sealed override string Filter(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                return path;
+            }
+            else
+            {
+                if (!File.Exists(path))
+                {
+                    return null;
+                }
+                return base.Filter(path);
+            }
         }
     }
 }
