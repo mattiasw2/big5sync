b2a4f48157c701a1a4cc76ac88c5e3ecfd3c344c
ericnjf <ericnjf@gmail.com>
2010-03-10 04:14:09 +0000

    added post traverse method

---
+++  big5sync/Syncless/Syncless/Core/Exceptions/UnhandledException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Syncless.Core.Exceptions
+{
+    public class UnhandledException :Exception
+    {
+        public UnhandledException()
+        {
+
+        }
+
+    }
+}
---  big5sync/Syncless/Syncless/Tagging/TaggingXMLHelper.cs
+++  big5sync/Syncless/Syncless/Tagging/TaggingXMLHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using System.IO;
-
+using Syncless.Filters;
 namespace Syncless.Tagging
 {
     static class TaggingXMLHelper
@@ -26,30 +26,33 @@ namespace Syncless.Tagging
         private const string ELE_TAGGED_FOLDER_UPDATED = "updated";
         private const string ELE_TAGGED_FOLDER_PATH = "path";
 
+        private const string ELE_FILTER_ROOT = "filters";
+        private const string ELE_FILTER_CHILD_FILTER = "filter";
+
+        private const string ELE_FILTER_TYPE = "type";
+        private const string ELE_FILTER_MODE = "mode";
 
 
+        //Type of Mode
+        private const string ELE_FILTER_MODE_INCLUDE = "Include";
+        private const string ELE_FILTER_MODE_EXCLUDE = "Exclude";
 
+        //Type of Filter
+        #region Extension
+        private const string ELE_FILTER_TYPE_EXT = "Extension";
+        private const string ELE_FILTER_TYPE_EXT_PATTERN = "pattern";
+        #endregion
 
-
-
-
-
-        private static XmlDocument ConvertTaggingProfileToXml(TaggingProfile taggingProfile)
+        public static void SaveTo(TaggingProfile taggingProfile, List<string> xmlFilePaths)
         {
-            XmlDocument TaggingDataDocument = new XmlDocument();
-            XmlElement taggingElement = TaggingDataDocument.CreateElement(ELE_PROFILE_ROOT);
-            XmlElement profileElement = TaggingDataDocument.CreateElement(ELE_PROFILE);
-            profileElement.SetAttribute(ELE_PROFILE_NAME, taggingProfile.ProfileName);
-            profileElement.SetAttribute(ELE_PROFILE_CREATEDDATE, taggingProfile.Created.ToString());
-            profileElement.SetAttribute(ELE_PROFILE_LASTUPDATEDDATE, taggingProfile.LastUpdated.ToString());
-            foreach (Tag tag in taggingProfile.TagList)
+            XmlDocument xml = ConvertTaggingProfileToXml(taggingProfile);
+
+            foreach (string path in xmlFilePaths)
             {
-                profileElement.AppendChild(TaggingXMLHelper.CreateTagElement(TaggingDataDocument, tag));
+                TaggingXMLHelper.SaveXml(xml, path);
             }
-            taggingElement.AppendChild(profileElement);
-            TaggingDataDocument.AppendChild(taggingElement);
-            return TaggingDataDocument;
         }
+
 
         private static TaggingProfile ConvertXmlToTaggingProfile(XmlDocument xml)
         {
@@ -67,15 +70,7 @@ namespace Syncless.Tagging
             return taggingProfile;
         }
 
-        public static void SaveTo(TaggingProfile taggingProfile , List<string> xmlFilePaths)
-        {
-            XmlDocument xml = ConvertTaggingProfileToXml(taggingProfile);
-
-            foreach (string path in xmlFilePaths)
-            {
-                TaggingXMLHelper.SaveXml(xml, path);
-            }
-        }
+        
 
         public static TaggingProfile LoadFrom(string xmlFilePath)
         {
@@ -92,7 +87,7 @@ namespace Syncless.Tagging
             }
         }
 
-        internal static bool SaveXml(XmlDocument xml, string path)
+        private static bool SaveXml(XmlDocument xml, string path)
         {
             XmlTextWriter textWriter = null;
             FileStream fs = null;
@@ -123,7 +118,7 @@ namespace Syncless.Tagging
             return true;
         }
 
-        internal static XmlDocument LoadXml(string path)
+        private static XmlDocument LoadXml(string path)
         {
             FileStream fs = null;
             try
@@ -153,7 +148,7 @@ namespace Syncless.Tagging
         }
 
         #region create tagging profile
-        internal static TaggingProfile CreateTaggingProfile(XmlElement profileElement)
+        private static TaggingProfile CreateTaggingProfile(XmlElement profileElement)
         {
             TaggingProfile taggingProfile = new TaggingProfile();
             string profilename = profileElement.GetAttribute(ELE_PROFILE_NAME);
@@ -165,14 +160,14 @@ namespace Syncless.Tagging
             return taggingProfile;
         }
 
-        internal static Tag CreateTagFromXml(XmlElement tagElement)
+        private static Tag CreateTagFromXml(XmlElement tagElement)
         {
             string tagname = tagElement.GetAttribute(ELE_TAG_NAME);
             long created = long.Parse(tagElement.GetAttribute(ELE_TAG_CREATEDDATE));
             long lastupdated = long.Parse(tagElement.GetAttribute(ELE_TAG_UPDATEDDATE));
             Tag tag = new Tag(tagname, created);
             tag.LastUpdated = lastupdated;
-            XmlNodeList pathList = tagElement.ChildNodes;
+            XmlNodeList pathList = tagElement.GetElementsByTagName(ELE_TAGGED_FOLDER);
             foreach (XmlElement path in pathList)
             {
                 TaggedPath taggedPath = CreatePath(path);
@@ -202,10 +197,72 @@ namespace Syncless.Tagging
             taggedPath.LogicalDriveId = TaggingHelper.GetLogicalID(pathname);
             return taggedPath;
         }
+
+        private static List<Filter> LoadFilterList(XmlElement filterListNode)
+        {
+            List<Filter> filterList = new List<Filter>();
+            XmlNodeList elementNodes = filterListNode.GetElementsByTagName(ELE_FILTER_CHILD_FILTER);
+            foreach (XmlNode n in elementNodes)
+            {
+                Filter f = LoadFilter((XmlElement)n);
+                filterList.Add(f);
+            }
+            return filterList;
+        }
+
+        private static Filter LoadFilter(XmlElement filterNode)
+        {
+            
+            string type = filterNode.GetAttribute(ELE_FILTER_TYPE);
+            string mode = filterNode.GetAttribute(ELE_FILTER_MODE);
+            FilterMode fMode = FilterMode.INCLUDE;
+            if (mode.Equals(ELE_FILTER_MODE_EXCLUDE))
+            {
+                fMode = FilterMode.EXCLUDE;
+            }
+            else if (mode.Equals(ELE_FILTER_MODE_INCLUDE))
+            {
+                fMode = FilterMode.INCLUDE;
+            }
+            else
+            {
+                //CNANOT HAPPEN!!!!!
+            }
+            if (type.Equals(ELE_FILTER_TYPE_EXT))
+            {
+                XmlNodeList list = filterNode.GetElementsByTagName(ELE_FILTER_TYPE_EXT_PATTERN);
+                if (list.Count == 0)
+                {
+                    //THROW EXCEPTION
+                }
+                XmlElement ext = (XmlElement)list[0];
+
+                ExtensionFilter filter = new ExtensionFilter(ext.InnerText, fMode);
+                return filter;
+            }
+            return null;
+        }
         #endregion
 
         #region create xml document
-        internal static XmlElement CreateTagElement(XmlDocument TaggingDataDocument, Tag tag)
+        private static XmlDocument ConvertTaggingProfileToXml(TaggingProfile taggingProfile)
+        {
+            XmlDocument TaggingDataDocument = new XmlDocument();
+            XmlElement taggingElement = TaggingDataDocument.CreateElement(ELE_PROFILE_ROOT);
+            XmlElement profileElement = TaggingDataDocument.CreateElement(ELE_PROFILE);
+            profileElement.SetAttribute(ELE_PROFILE_NAME, taggingProfile.ProfileName);
+            profileElement.SetAttribute(ELE_PROFILE_CREATEDDATE, taggingProfile.Created.ToString());
+            profileElement.SetAttribute(ELE_PROFILE_LASTUPDATEDDATE, taggingProfile.LastUpdated.ToString());
+            foreach (Tag tag in taggingProfile.TagList)
+            {
+                profileElement.AppendChild(TaggingXMLHelper.CreateTagElement(TaggingDataDocument, tag));
+            }
+            taggingElement.AppendChild(profileElement);
+            TaggingDataDocument.AppendChild(taggingElement);
+            return TaggingDataDocument;
+        }
+
+        private static XmlElement CreateTagElement(XmlDocument TaggingDataDocument, Tag tag)
         {
             XmlElement tagElement = TaggingDataDocument.CreateElement(ELE_TAG);
             tagElement.SetAttribute(ELE_TAG_NAME, tag.TagName);
@@ -215,6 +272,7 @@ namespace Syncless.Tagging
             {
                 tagElement.AppendChild(CreateTaggedFolderElement(TaggingDataDocument, path));
             }
+            tagElement.AppendChild(CreateFilterElementList(TaggingDataDocument, tag.Filters));
             return tagElement;
         }
 
@@ -228,6 +286,38 @@ namespace Syncless.Tagging
             taggedFolderElement.AppendChild(folderPathElement);
             return taggedFolderElement;
         }
+
+        private static XmlElement CreateFilterElementList(XmlDocument xmlDoc , List<Filter> filters){
+            XmlElement filterRoot = xmlDoc.CreateElement(ELE_FILTER_ROOT);
+            foreach (Filter f in filters)
+            {
+                XmlElement filterEle = CreateFilterElement(xmlDoc, f);
+                filterRoot.AppendChild(filterEle);
+            }
+            return filterRoot;
+        }
+
+        private static XmlElement CreateFilterElement(XmlDocument xmlDoc , Filter filter){
+            XmlElement filterElement = xmlDoc.CreateElement(ELE_FILTER_CHILD_FILTER);
+            string mode = filter.Mode==FilterMode.INCLUDE?ELE_FILTER_MODE_INCLUDE:ELE_FILTER_MODE_EXCLUDE;
+            filterElement.SetAttribute(ELE_FILTER_MODE, mode);
+            if (filter is ExtensionFilter)
+            {
+                string type = ELE_FILTER_TYPE_EXT;
+                filterElement.SetAttribute(ELE_FILTER_TYPE, type);
+                XmlElement ext = xmlDoc.CreateElement(ELE_FILTER_TYPE_EXT_PATTERN);
+                ext.InnerText = ((ExtensionFilter)filter).Pattern;
+                filterElement.AppendChild(ext);
+            }
+            return filterElement;
+        }
         #endregion
+
+
+
+
+
+
+        
     }
 }
