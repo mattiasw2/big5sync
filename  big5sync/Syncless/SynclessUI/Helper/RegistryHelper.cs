b7c5f319ce60a03194566f51dce0bef4b6c1e415
xylancer <xylancer@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-12 10:06:17 +0000

    Remove File Shell from RegistryHelper

---  big5sync/Syncless/SynclessUI/Helper/RegistryHelper.cs
+++  big5sync/Syncless/SynclessUI/Helper/RegistryHelper.cs
@@ -13,24 +13,7 @@ namespace SynclessUI.Helper
         /// <param name="path">The location of Syncless</param>
         public static void CreateRegistry(string path)
         {
-            CreateRegistryForFile(path);
             CreateRegistryForFolder(path);
-        }
-
-        public static void CreateRegistryForFile(string path)
-        {
-            RegistryKey tagKey = Registry.CurrentUser.CreateSubKey(@"Software\Classes\*\shell\Tag");
-            tagKey.SetValue(null, "Tag");
-
-            RegistryKey tagKeyCommand = Registry.CurrentUser.CreateSubKey(@"Software\Classes\*\shell\Tag\command");
-            tagKeyCommand.SetValue(null, path + " -TFile %1");
-
-            RegistryKey untagKey = Registry.CurrentUser.CreateSubKey(@"Software\Classes\*\shell\Untag");
-            untagKey.SetValue(null, "Untag");
-
-            RegistryKey untagKeyCommand = Registry.CurrentUser.CreateSubKey(@"Software\Classes\*\shell\Untag\command");
-            untagKeyCommand.SetValue(null, path + " -UTFile %1");
-
         }
 
         public static void CreateRegistryForFolder(string path)
@@ -49,9 +32,6 @@ namespace SynclessUI.Helper
         }
         public static void RemoveRegistry()
         {
-
-            Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\*\shell\Tag");
-            Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\*\shell\Untag");
             Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\Folder\shell\Tag");
             Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\Folder\shell\Untag");
         }
