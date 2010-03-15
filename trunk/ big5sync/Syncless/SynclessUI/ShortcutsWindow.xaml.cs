a35ed2ea079ce9f167429e3672739e7569c5fb87
xylancer <xylancer@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-15 02:42:07 +0000

    UI: Set Opacity of all Sub-Windows to 95%. Added Shortcuts Window.

---
+++ trunk/ big5sync/Syncless/SynclessUI/ShortcutsWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SynclessUI
+{
+    /// <summary>
+    /// Interaction logic for ShortcutsWindow.xaml
+    /// </summary>
+    public partial class ShortcutsWindow : Window
+    {
+		public ShortcutsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+        	this.DragMove();
+        }
+
+		private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			this.Close();
+		}
+    }
+}
