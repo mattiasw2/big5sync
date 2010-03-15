548d7707d47db1359d2c233ad5be3084c5b81782
ericnjf@gmail.com <ericnjf@gmail.com@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-15 12:05:50 +0000

    bug fix

---
+++ trunk/ big5sync/Syncless/Syncless/Helper/CommonXmlHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+namespace Syncless.Helper
+{
+    public static class CommonXmlHelper
+    {
+        public static void SaveXml(XmlDocument xml, string path)
+        {
+            XmlTextWriter textWriter = null;
+            FileStream fs = null;
+
+            FileInfo fileInfo = new FileInfo(path);
+            if (!fileInfo.Directory.Exists)
+            {
+                DirectoryInfo info = Directory.CreateDirectory(fileInfo.Directory.FullName);
+                info.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+            }
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                fs = new FileStream(path, FileMode.OpenOrCreate);
+                textWriter = new XmlTextWriter(fs, Encoding.UTF8);
+                textWriter.Formatting = Formatting.Indented;
+                xml.WriteContentTo(textWriter);
+            }
+            catch (IOException io)
+            {
+                //TODO : Error Log
+                Console.WriteLine(io.StackTrace);
+            }
+            finally
+            {
+                if (textWriter != null)
+                {
+                    try
+                    {
+                        textWriter.Close();
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+            }
+
+        }
+
+        public static XmlDocument LoadXml(string path)
+        {
+            FileStream fs = null;
+            try
+            {
+                FileInfo info = new FileInfo(path);
+                if (!info.Exists)
+                {
+                    return null;
+                }
+                fs = info.Open(FileMode.Open);
+                XmlDocument xml = new XmlDocument();
+                xml.Load(fs);
+                return xml;
+            }
+            catch (XmlException xml)
+            {
+                Core.ExceptionHandler.Handle(xml);
+                return null;
+            }
+            catch (IOException io)
+            {
+                Core.ExceptionHandler.Handle(io);
+                return null;
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    try { fs.Close(); }
+                    catch (Exception) { }
+                }
+            }
+
+        }
+    }
+}
