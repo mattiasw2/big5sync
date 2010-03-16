781480f34c00cf8349891985d40b48a4314882f5
sohyuanchin <sohyuanchin@gmail.com>
2010-03-16 11:35:41 +0000

    Commented out creation time for folder comparison for 0.9. Buggy.

---  big5sync/Syncless/Syncless/CompareAndSync/CompareObject/FolderCompareObject.cs
+++  big5sync/Syncless/Syncless/CompareAndSync/CompareObject/FolderCompareObject.cs
@@ -61,7 +61,7 @@ namespace Syncless.CompareAndSync.CompareObject
             {
                 if ((f = objects.ElementAt(i) as FolderCompareObject) != null)
                 {
-                    if (f.MetaName != null && f.MetaName == name && f.Name != name && f.CreationTime[pos] == creationTime && f.ChangeType[pos] == MetaChangeType.New)
+                    if (f.MetaName != null && f.MetaName == name && f.Name != name && /*f.CreationTime[pos] == creationTime &&*/ f.ChangeType[pos] == MetaChangeType.New)
                     {
                         result = f;
                         counter++;
