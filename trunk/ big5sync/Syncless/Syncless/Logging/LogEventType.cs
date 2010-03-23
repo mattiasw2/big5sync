fdf3936ff4f6d41cae1746f2571578622c07e546
kohcherguan <kohcherguan@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-23 14:25:51 +0000

    Some User Logger enhancement

---
+++ trunk/ big5sync/Syncless/Syncless/Logging/LogEventType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Syncless.Logging
+{
+    public enum LogEventType
+    {
+        SYNC_START,
+        SYNC_STOP,
+        APPEVENT_DRIVE_ADDED,
+        APPEVENT_DRIVE_RENAMED,
+        APPEVENT_TAG_CREATED,
+        APPEVENT_TAG_DELETED,
+        APPEVENT_TAG_CONFIG_UPDATED,
+        APPEVENT_FOLDER_TAGGED,
+        APPEVENT_FOLDER_UNTAGGED,
+        FSCHANGE_CREATED,
+        FSCHANGE_MODIFIED,
+        FSCHANGE_DELETED,
+        FSCHANGE_RENAMED
+    }
+}
