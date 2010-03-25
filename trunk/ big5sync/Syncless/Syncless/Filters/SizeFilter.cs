f5aff00fb50459e18ecd652fedb68dd0f775aefd
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-25 11:42:12 +0000

    created SizeFilter class, not yet implemented

---
+++ trunk/ big5sync/Syncless/Syncless/Filters/SizeFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Syncless.Filters
+{
+    public class SizeFilter : FileFilter
+    {
+        public SizeFilter(FilterMode mode) : base(mode) {}
+        public override bool Match(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
