e604d8e72cc83e45f88095307e843ec7756d99c8
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-25 10:04:04 +0000

    refactor tagginglayer

---  big5sync/Syncless/Syncless/Tagging/TagMerger.cs
+++  big5sync/Syncless/Syncless/Tagging/TagMerger.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Syncless.Core;
-using Syncless.Notification;
 using Syncless.Notification.SLLNotification;
 namespace Syncless.Tagging
 {
