cf264a16b27c161102fbb9bfbf2b5c0708a87160
kohcherguan <kohcherguan@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-26 13:42:17 +0000

    Developer Logger Up

---
+++ trunk/ big5sync/Syncless/Syncless/Logging/DeveloperLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using Syncless.Core;
+
+namespace Syncless.Logging
+{
+    public class DeveloperLogger : Logger
+    {
+
+        public DeveloperLogger()
+            : base(ServiceLocator.DEVELOPER_LOG)
+        {
+        }
+
+        public override void Write(object message)
+        {
+            Debug.Assert(message is String);
+            string msg = message as String;
+            log.Info(msg);
+        }
+    }
+}
