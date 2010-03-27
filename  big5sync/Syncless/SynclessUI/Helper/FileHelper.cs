cdcec46c4f1f389e7e0b0317cecb17a55eb92caf
xylancer <xylancer@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-27 09:15:24 +0000

    Polished Up UI, Changed some shortcuts to more appropriate ones, implemented folder shortcuts detection.

---  big5sync/Syncless/SynclessUI/Helper/FileHelper.cs
+++  big5sync/Syncless/SynclessUI/Helper/FileHelper.cs
@@ -18,5 +18,30 @@ namespace SynclessUI.Helper
                 return false;
             }
         }
+
+        // Credits of http://www.saunalahti.fi/janij/blog/2006-12.html
+
+        public static string GetShortcutTargetFile(string shortcutFilename)
+        {
+            string extension = System.IO.Path.GetExtension(shortcutFilename);
+
+            if(extension.ToLower() == ".lnk")
+            {
+                string pathOnly = System.IO.Path.GetDirectoryName(shortcutFilename);
+                string filenameOnly = System.IO.Path.GetFileName(shortcutFilename);
+
+                Shell32.Shell shell = new Shell32.ShellClass();
+                Shell32.Folder folder = shell.NameSpace(pathOnly);
+                Shell32.FolderItem folderItem = folder.ParseName(filenameOnly);
+                if (folderItem != null)
+                {
+                    Shell32.ShellLinkObject link =
+                (Shell32.ShellLinkObject)folderItem.GetLink;
+                    return link.Path;
+                }
+            }
+
+            return null; // not found
+        }
     }
 }
