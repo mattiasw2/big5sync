41fc2c589b5a8b6fba2a90fa3f9ba49a2a684683
ericnjf@gmail.com <ericnjf@gmail.com@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-29 02:00:30 +0000

    Untag now clean the folder's Metadata if  1) the folder is not tag to another folder  2) no parent of the folder is tagged  3) if a child is tagged, its metadata will not be cleared.

--- trunk/ big5sync/Syncless/Syncless/Helper/Cleaner.cs
+++ trunk/ big5sync/Syncless/Syncless/Helper/Cleaner.cs
@@ -29,5 +29,38 @@ namespace Syncless.Helper
             }
             return count;
         }
+        public static int CleanSynclessMeta(DirectoryInfo info, List<string> childPaths)
+        {
+            DirectoryInfo[] infos = info.GetDirectories("*", SearchOption.AllDirectories);
+            int count = 0;
+            foreach (DirectoryInfo i in infos)
+            {
+                if (i.Name.ToLower().Equals(".syncless"))
+                {
+                    bool isChild = false;
+                    foreach (string path in childPaths)
+                    {
+                        if (i.FullName.ToLower().Contains(path.ToLower()))
+                        {
+                            isChild = true;
+                            break;
+                        }
+                    }
+                    if (isChild)
+                    {
+                        continue;
+                    }
+                    try
+                    {
+                        i.Delete(true);
+                        count++;
+                    }
+                    catch (IOException)
+                    {
+                    }
+                }
+            }
+            return count;
+        }
     }
 }
