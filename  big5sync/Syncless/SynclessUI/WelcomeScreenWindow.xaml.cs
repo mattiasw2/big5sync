545e16c60dcb1c408c032d9585678ab50e0c330b
xylancer <xylancer@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-03-31 11:25:15 +0000

    commited

---
+++  big5sync/Syncless/SynclessUI/WelcomeScreenWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Syncless.Core.Exceptions;
+using SynclessUI.Helper;
+
+namespace SynclessUI
+{
+    /// <summary>
+    /// Interaction logic for CreateTagWindow.xaml
+    /// </summary>
+    public partial class WelcomeScreenWindow : Window
+    {
+        private bool _closingAnimationNotCompleted = true;
+        private MainWindow _main;
+
+        public WelcomeScreenWindow(MainWindow main)
+        {
+            InitializeComponent();
+
+            _main = main;
+			Show();
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            BtnOk.IsEnabled = false;
+            Close();
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void FormFadeOut_Completed(object sender, EventArgs e)
+        {
+            _closingAnimationNotCompleted = false;
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_closingAnimationNotCompleted)
+            {
+                e.Cancel = true;
+                FormFadeOut.Begin();
+            }
+        }
+    }
+}
