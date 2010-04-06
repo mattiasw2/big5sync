a5fc4d589a4e7f676a67d0439b5ac22e2c0994a0
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-06 07:03:03 +0000

    added comment to TagConfig.cs

---  big5sync/Syncless/Syncless/Tagging/TagConfig.cs
+++  big5sync/Syncless/Syncless/Tagging/TagConfig.cs
@@ -1,16 +1,26 @@
 ﻿
 namespace Syncless.Tagging
 {
+    /// <summary>
+    /// TagConfig class provides the configuration options of <see cref="Tag">Tag</see> object
+    /// </summary>
     internal class TagConfig
     {
         private bool _isSeamless;
 
+        /// <summary>
+        /// Gets or sets the _isSeamless attribute of a <see cref="Tag">Tag</see> object
+        /// </summary>
         public bool IsSeamless
         {
             get { return _isSeamless; }
             set { _isSeamless = value; }
         }
         
+        /// <summary>
+        /// Creates a new TagConfig object
+        /// </summary>
+        /// <remarks>The IsSeamless property is by default False</remarks>
         public TagConfig()
         {
             _isSeamless = false;
