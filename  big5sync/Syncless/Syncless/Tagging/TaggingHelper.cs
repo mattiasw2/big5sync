8dc575becedd5321960b95242df1a1d619be460f
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-06 12:38:03 +0000

    added comment to TaggingHelper.cs

---  big5sync/Syncless/Syncless/Tagging/TaggingHelper.cs
+++  big5sync/Syncless/Syncless/Tagging/TaggingHelper.cs
@@ -5,8 +5,18 @@ using Syncless.Core;
 
 namespace Syncless.Tagging
 {
-    static class TaggingHelper
+    /// <summary>
+    /// TaggingHelper provides some common methods which may be used by other classes under the 
+    /// Tagging namespace
+    /// </summary>
+    internal static class TaggingHelper
     {
+        /// <summary>
+        /// Removes the trailing element of a string array if it contains an empty string
+        /// </summary>
+        /// <param name="tempPathTokens">The string array to be trimed</param>
+        /// <returns>the trimed string array if its trailing element contains an empty string;
+        /// otherwise, the string array that is passed as parameter</returns>
         internal static string[] TrimEnd(string[] tempPathTokens)
         {
             string[] pathTokens = new string[tempPathTokens.Length - 1];
@@ -21,22 +31,42 @@ namespace Syncless.Tagging
             return tempPathTokens;
         }
 
+        /// <summary>
+        /// Converts a tag name to lower case
+        /// </summary>
+        /// <param name="tagname">The string value that represents the tag name to be formatted</param>
+        /// <returns>the tag name that is converted to lower case</returns>
         internal static string FormatTagName(string tagname)
         {
             return tagname.ToLower();
         }
 
+        /// <summary>
+        /// Gets the logical ID of the path that is passed as parameter
+        /// </summary>
+        /// <param name="path">The string value that represents the path whose logical ID is to be retrieved
+        /// </param>
+        /// <returns>the logical ID of the path that is passed as parameter</returns>
         internal static string GetLogicalID(string path)
         {
             string[] tokens = path.Split('\\');
             return (tokens[0].TrimEnd(':'));
         }
 
+        /// <summary>
+        /// Gets the number of ticks that represents the current date time
+        /// </summary>
+        /// <returns>the current date time in long format</returns>
         internal static long GetCurrentTime()
         {
             return DateTime.Now.Ticks;
         }
 
+        /// <summary>
+        /// Gets the string format of the current date time
+        /// </summary>
+        /// <returns>the current date time in string format</returns>
+        /// <remarks>DD/MM/YY HH:MM:SS</remarks>
         internal static string GetCurrentTimeString()
         {
             DateTime currenttime = DateTime.Now;
@@ -50,6 +80,15 @@ namespace Syncless.Tagging
             return datestring;
         }
 
+        /// <summary>
+        /// Creates a path from a string array starting from the element at position indicated by the trailing
+        /// index that is passed as parameter
+        /// </summary>
+        /// <param name="trailingIndex">The integer value that represents the index of the element of the
+        /// string array at which the path is to be created from</param>
+        /// <param name="pathTokens">The string array that contains string values which will be used
+        /// to create the path string</param>
+        /// <returns>the string value that is created from the string array</returns>
         internal static string CreatePath(int trailingIndex, string[] pathTokens)
         {
             string trailingPath = null;
@@ -61,6 +100,15 @@ namespace Syncless.Tagging
             return trailingPath;
         }
 
+        /// <summary>
+        /// Checks whether a tag contains a tagged path whose full path name is parent path or a child path
+        /// of the path that is passed as parameter
+        /// </summary>
+        /// <param name="tag">The <see cref="Tag">Tag</see> object that represents the tag whose list
+        /// of tagged paths is to be checked</param>
+        /// <param name="path">The string value that represents the path that is to be used to check</param>
+        /// <returns>true if the tag contains a tagged path whose full path name is a parent path or a 
+        /// child path of the path that is passed as parameter; otherwise, false</returns>
         internal static bool CheckRecursiveDirectory(Tag tag, string path)
         {
             foreach (TaggedPath p in tag.FilteredPathList)
@@ -76,6 +124,16 @@ namespace Syncless.Tagging
             return false;
         }
 
+        /// <summary>
+        /// Checks the last position of two string arrays who have the first few elements containing the
+        /// same string value
+        /// </summary>
+        /// <param name="childPathTokens">The string array with same number or less number of string
+        /// elements</param>
+        /// <param name="parentPathTokens">The string array with same number or more number of string
+        /// elements</param>
+        /// <returns>the index at which the elements at position before this index are equal in both
+        /// string arrays</returns>
         internal static int Match(string[] childPathTokens, string[] parentPathTokens)
         {
             int trailingIndex = 0;
@@ -93,6 +151,14 @@ namespace Syncless.Tagging
             return trailingIndex;
         }
 
+        /// <summary>
+        /// Logs a message to a log file through <see cref="ServiceLocator.GetLogger">ServiceLocator.GetLogger
+        /// </see>
+        /// </summary>
+        /// <param name="eventType">The <see cref="LogEventType">LogEventType</see> of the log message</param>
+        /// <param name="message">The string value that represents the message to be logged</param>
+        /// <param name="list">The list of parameters that will replace the format item in the message
+        /// string that is passed as parameter</param>
         internal static void Logging(LogEventType eventType, string message, params object[] list)
         {
             ServiceLocator.GetLogger(ServiceLocator.USER_LOG).Write(new LogData(eventType, string.Format(message, list)));
