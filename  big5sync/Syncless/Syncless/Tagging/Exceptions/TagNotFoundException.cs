c8529864d99a7ddb386e1874a05ead24bcbf2dd1
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 07:48:54 +0000

    added comment to FilterAlreadyExistException.cs added comment to TagNotFoundException.cs

---  big5sync/Syncless/Syncless/Tagging/Exceptions/TagNotFoundException.cs
+++  big5sync/Syncless/Syncless/Tagging/Exceptions/TagNotFoundException.cs
@@ -6,22 +6,37 @@ using Syncless.Helper;
 
 namespace Syncless.Tagging.Exceptions
 {
+    /// <summary>
+    /// The exception that is thrown when attempt retrieve a tag that does not exist.
+    /// </summary>
     public class TagNotFoundException : Exception
     {
         private string _tagname;
 
+        /// <summary>
+        /// Gets the name of the tag
+        /// </summary>
         public string Tagname
         {
             get { return _tagname; }
         }
         
+        /// <summary>
+        /// Initializes a new instance of the TagNotFoundException class with its message string 
+        /// set to a system-supplied message
+        /// </summary>
         public TagNotFoundException()
             : base()
         {
         }
 
-        
-
+        /// <summary>
+        /// Initializes a new instance of the TagNotFoundException class with its message string 
+        /// set to message that is given by <see cref="ErrorMessage.TAG_NOT_FOUND_EXCEPTION">
+        /// ErrorMessage.TAG_NOT_FOUND_EXCEPTION</see> string value and the tag name property to 
+        /// the tag name that is passed as parameter
+        /// </summary>
+        /// <param name="tagname">The string value that represents the tag name</param>
         public TagNotFoundException(string tagname)
             : base(ErrorMessage.TAG_NOT_FOUND_EXCEPTION)
         {
