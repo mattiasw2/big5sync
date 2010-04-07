5bc82b6fb906bc2993dd7e30d5c7baa8c10fa320
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 07:42:13 +0000

    added comment to ErrorMessage.cs for tagging error message added comment to TagAlreadyExistsException.cs added comment to RecursiveDirectoryException.cs added comment to PathNotFoundException.cs added comment to PathAlreadyExistsException.cs added comment to FilterNotExistException.cs added comment to FilterAlreadyExistsException.cs

---  big5sync/Syncless/Syncless/Tagging/Exceptions/PathAlreadyExistsException.cs
+++  big5sync/Syncless/Syncless/Tagging/Exceptions/PathAlreadyExistsException.cs
@@ -6,21 +6,37 @@ using Syncless.Helper;
 
 namespace Syncless.Tagging.Exceptions
 {
+    /// <summary>
+    /// The exception that is thrown when attempt to tag a path that already exists.
+    /// </summary>
     public class PathAlreadyExistsException : Exception
     {
         private string _pathname;
 
+        /// <summary>
+        /// Gets the full name of the path
+        /// </summary>
         public string Pathname
         {
             get { return _pathname; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the PathAlreadyExistsException class with its message string 
+        /// set to a system-supplied message
+        /// </summary>
         public PathAlreadyExistsException()
             : base()
         {
         }
 
-
+        /// <summary>
+        /// Initializes a new instance of the PathAlreadyExistsException class with its message string 
+        /// set to message that is given by <see cref="ErrorMessage.PATH_ALREADY_EXISTS_EXCEPTION">
+        /// ErrorMessage.PATH_ALREADY_EXISTS_EXCEPTION</see> string value and the path property to 
+        /// the path name that is passed as parameter
+        /// </summary>
+        /// <param name="pathname">The string value that represents the path name</param>
         public PathAlreadyExistsException(string pathname)
             : base(ErrorMessage.PATH_ALREADY_EXISTS_EXCEPTION)
         {
---  big5sync/Syncless/Syncless/Tagging/Exceptions/TagAlreadyExistsException.cs
+++  big5sync/Syncless/Syncless/Tagging/Exceptions/TagAlreadyExistsException.cs
@@ -6,21 +6,37 @@ using Syncless.Helper;
 
 namespace Syncless.Tagging.Exceptions
 {
+    /// <summary>
+    /// The exception that is thrown when attempt to create a tag that already exists.
+    /// </summary>
     public class TagAlreadyExistsException : Exception
     {
         private string _tagname;
 
+        /// <summary>
+        /// Gets the name of the tag
+        /// </summary>
         public string Tagname
         {
             get { return _tagname; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the TagAlreadyExistsException class with its message string 
+        /// set to a system-supplied message
+        /// </summary>
         public TagAlreadyExistsException()
             : base()
         {
         }
 
-
+        /// <summary>
+        /// Initializes a new instance of the TagAlreadyExistsException class with its message string 
+        /// set to message that is given by <see cref="ErrorMessage.TAG_ALREADY_EXISTS_EXCEPTION">
+        /// ErrorMessage.TAG_ALREADY_EXISTS_EXCEPTION</see> string value and the tag name property to 
+        /// the tag name that is passed as parameter
+        /// </summary>
+        /// <param name="tagname">The string value that represents the tag name</param>
         public TagAlreadyExistsException(string tagname)
             : base(ErrorMessage.TAG_ALREADY_EXISTS_EXCEPTION)
         {
