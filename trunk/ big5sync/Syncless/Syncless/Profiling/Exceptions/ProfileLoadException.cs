cb6cc69c3513b82c2ad9107d650e541754d3b8d8
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 09:20:17 +0000

    added comment to ProfileLoadException.cs

--- trunk/ big5sync/Syncless/Syncless/Profiling/Exceptions/ProfileLoadException.cs
+++ trunk/ big5sync/Syncless/Syncless/Profiling/Exceptions/ProfileLoadException.cs
@@ -5,12 +5,25 @@ using System.Text;
 
 namespace Syncless.Profiling.Exceptions
 {
-    public class ProfileLoadException:Exception
+    /// <summary>
+    /// The exception that is thrown when an attempt to load a profile fails.
+    /// </summary>
+    public class ProfileLoadException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the ProfileLoadException class with its message 
+        /// string set to a system-supplied message 
+        /// </summary>
+        /// <param name="e">The inner exception that causes this exception</param>
         public ProfileLoadException(Exception e)
-            : base(e.Message,e)
+            : base(e.Message, e)
         {
         }
+
+        /// <summary>
+        /// Initializes a new instance of the ProfileLoadException class with its message 
+        /// string set to a system-supplied message
+        /// </summary>
         public ProfileLoadException()
         {
 
