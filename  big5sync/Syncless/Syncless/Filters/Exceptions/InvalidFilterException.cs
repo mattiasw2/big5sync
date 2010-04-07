b3a61d13a6565748c83bb938bf54556a803a2fff
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 09:35:44 +0000

    added comment to InvalidFilterException.cs

---  big5sync/Syncless/Syncless/Filters/Exceptions/InvalidFilterException.cs
+++  big5sync/Syncless/Syncless/Filters/Exceptions/InvalidFilterException.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace Syncless.Filters.Exceptions
 {
+    /// <summary>
+    /// The exception that is thrown when a filter is invalid.
+    /// </summary>
     public class InvalidFilterException : Exception 
     {
     }
