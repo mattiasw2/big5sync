9f6a5c9dd068ee5caf2e0377b0b1d48961880b74
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 08:02:26 +0000

    added comment to PathHelper.cs

--- trunk/ big5sync/Syncless/Syncless/Helper/PathHelper.cs
+++ trunk/ big5sync/Syncless/Syncless/Helper/PathHelper.cs
@@ -5,8 +5,17 @@ using System.Text;
 
 namespace Syncless.Helper
 {
+    /// <summary>
+    /// PatHelper class provides operations to manipulate path string values
+    /// </summary>
     public class PathHelper
     {
+        /// <summary>
+        /// Converts a path name to lower case and appends a '\' character to the end of the path name if it
+        /// does not exist
+        /// </summary>
+        /// <param name="path">The string value that represents the path name to be formatted</param>
+        /// <returns>the formatted path name</returns>
         public static string FormatFolderPath(string path)
         {
             if (path.EndsWith("\\"))
@@ -20,11 +29,21 @@ namespace Syncless.Helper
             }
         }
 
+        /// <summary>
+        /// Converts a path name to lower case
+        /// </summary>
+        /// <param name="path">The string value that represents the path name to be formatted</param>
+        /// <returns>the formatted path name</returns>
         public static string FormatPath(string path)
         {
             return path.ToLower();
         }
 
+        /// <summary>
+        /// Removes the '\' character from the end of the path name if it exists
+        /// </summary>
+        /// <param name="path">The string value that represents the path name to be formatted</param>
+        /// <returns>the formatted path name</returns>
         public static string RemoveTrailingSlash(string path)
         {
             if (path.EndsWith("\\"))
@@ -34,6 +53,11 @@ namespace Syncless.Helper
             return path;
         }
 
+        /// <summary>
+        /// Appends the '\' character to the end of the path name if it does not exists
+        /// </summary>
+        /// <param name="path">The string value that represents the path name to be formatted</param>
+        /// <returns>the formatted path name</returns>
         public static string AddTrailingSlash(string path)
         {
             if (path.EndsWith("\\"))
@@ -47,6 +71,13 @@ namespace Syncless.Helper
             }
         }
 
+        /// <summary>
+        /// Checks if the path that is passed as parameter exists in the list of paths that is passed
+        /// as parameter
+        /// </summary>
+        /// <param name="pathlist">The list of strings that represents the list of paths</param>
+        /// <param name="path">The string value that represents the path to be searched</param>
+        /// <returns>true if the path exists in the list of paths; otherwise, false</returns>
         public static bool ContainsIgnoreCase(List<string> pathlist, string path)
         {
             foreach (string p in pathlist)
@@ -59,6 +90,12 @@ namespace Syncless.Helper
             return false;
         }
 
+        /// <summary>
+        /// Checks if two paths that are passed as parameters are equal
+        /// </summary>
+        /// <param name="p1">The string value that represents the path to be compared</param>
+        /// <param name="p2">The string value that represents the path to be compared</param>
+        /// <returns>true if the two paths are equal; otherwise, false</returns>
         public static bool EqualsIgnoreCase(string p1, string p2)
         {
             if (FormatFolderPath(p1).Equals(FormatFolderPath(p2)))
@@ -67,7 +104,15 @@ namespace Syncless.Helper
             }
             return false;
         }
-
+        
+        /// <summary>
+        /// Checks if the path name that is passed as first parameter has parent path that is equal to 
+        /// the path name that is passed as second parameter
+        /// </summary>
+        /// <param name="childpath">The string value that represents the longer path name</param>
+        /// <param name="parentpath">The string value that represents the shorter path name</param>
+        /// <returns>true if the path name that is passed as first parameter has parent path that is
+        /// equal to the path name that is passed as second parameter; otherwise, false</returns>
         public static bool StartsWithIgnoreCase(string childpath, string parentpath)
         {
             return FormatFolderPath(childpath).StartsWith(FormatFolderPath(parentpath));
