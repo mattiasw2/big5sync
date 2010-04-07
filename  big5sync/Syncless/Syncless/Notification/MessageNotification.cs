46ea438af6620d7a97eff94ebe3c5e72c499391f
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 10:50:36 +0000

    added comment to MessageNotification.cs

---  big5sync/Syncless/Syncless/Notification/MessageNotification.cs
+++  big5sync/Syncless/Syncless/Notification/MessageNotification.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace Syncless.Notification
 {
-    public class MessageNotification :AbstractNotification 
+    /// <summary>
+    /// MessageNotification class encloses properties for a notification sending message.
+    /// </summary>
+    public class MessageNotification : AbstractNotification
     {
+        /// <summary>
+        /// Creates a new MessageNotification object
+        /// </summary>
+        /// <param name="message">The string value that represents the message to be sent</param>
         public MessageNotification(string message)
             : base(message, Notification.NotificationCode.MessageNotification)
         {
