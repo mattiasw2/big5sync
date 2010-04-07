f3f00da7b64337e18743e55deae04407645026b1
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 14:31:24 +0000

    added comment to INotificationQueue.cs

---  big5sync/Syncless/Syncless/Notification/INotificationQueue.cs
+++  big5sync/Syncless/Syncless/Notification/INotificationQueue.cs
@@ -5,11 +5,36 @@ using System.Text;
 
 namespace Syncless.Notification
 {
+    /// <summary>
+    /// INotificationQueue is an interface for implementing a notification queue. Classes which implement the
+    /// interface must implement Enqueue, Dequeue, AddObserver and HasNotification methods.
+    /// </summary>
     public interface INotificationQueue 
     {
+        /// <summary>
+        /// Enqueues a <see cref="AbstractNotification">AbstractNotification</see> object
+        /// </summary>
+        /// <param name="notification">The <see cref="AbstractNotification">AbstractNotification</see> object 
+        /// that represents the notification to be enqueued</param>
+        /// <returns>true if the notification is enqueued; otherwise, false</returns>
         bool Enqueue(AbstractNotification notification);
+
+        /// <summary>
+        /// Dequeues a <see cref="AbstractNotification">AbstractNotification</see> object
+        /// </summary>
+        /// <returns>the first notification in the queue</returns>
         AbstractNotification Dequeue();
+
+        /// <summary>
+        /// Adds an <see cref="IQueueObserver">IQueueObserver</see> object to the list of observer objects
+        /// </summary>
+        /// <param name="obs"></param>
         void AddObserver(IQueueObserver obs);
+
+        /// <summary>
+        /// Checks if there is still notification objects in the queue
+        /// </summary>
+        /// <returns></returns>
         bool HasNotification();
     }
 }
