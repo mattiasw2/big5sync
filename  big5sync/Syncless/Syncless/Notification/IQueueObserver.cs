2e61d16e509ecaf2c1e78f1b11de1d72da6737c3
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 14:36:14 +0000

    added comment to IQueueObserver.cs

---  big5sync/Syncless/Syncless/Notification/IQueueObserver.cs
+++  big5sync/Syncless/Syncless/Notification/IQueueObserver.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace Syncless.Notification
 {
+    /// <summary>
+    /// IQueueObserver is an interface which provides a mechanism for receiving notifications from classes
+    /// implementing <see cref="INotificationQueue">INotificationQueue</see> interface.
+    /// </summary>
     public interface IQueueObserver
     {
+        /// <summary>
+        /// Notifies the observer that modification has occurred in the notification queue
+        /// </summary>
         void Update();
     }
 }
