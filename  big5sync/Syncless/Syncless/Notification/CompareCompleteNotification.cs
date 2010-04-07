364331a60f39adf49d835aff88060df4586cbdba
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 09:48:12 +0000

    added comment to CompareCompleteNotification.cs

---  big5sync/Syncless/Syncless/Notification/CompareCompleteNotification.cs
+++  big5sync/Syncless/Syncless/Notification/CompareCompleteNotification.cs
@@ -5,8 +5,14 @@ using System.Text;
 
 namespace Syncless.Notification.UINotification
 {
+    /// <summary>
+    /// CompareCompleteNotification class encloses properties for a notification for completing a compare.
+    /// </summary>
     public class CompareCompleteNotification : AbstractNotification
     {
+        /// <summary>
+        /// Creates a new CompareCompleteNotification object
+        /// </summary>
         public CompareCompleteNotification()
             : base("Compare Complete Notification", Syncless.Notification.NotificationCode.CompareCompleteNotification)
         {
