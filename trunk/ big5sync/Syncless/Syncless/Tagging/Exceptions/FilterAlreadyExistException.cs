96151c05a0d8262a7e10a4ea1b07bfd69c9375fa
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 07:48:54 +0000

    added comment to FilterAlreadyExistException.cs added comment to TagNotFoundException.cs

--- trunk/ big5sync/Syncless/Syncless/Tagging/Exceptions/FilterAlreadyExistException.cs
+++ trunk/ big5sync/Syncless/Syncless/Tagging/Exceptions/FilterAlreadyExistException.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Syncless.Filters;
+using Syncless.Helper;
 namespace Syncless.Tagging.Exceptions
 {
+    /// <summary>
+    /// The exception that is thrown when attempt to create a filter that already exists.
+    /// </summary>
     public class FilterAlreadyExistException : Exception
     {
         private Filter _filter;
 
+        /// <summary>
+        /// Gets or sets the filter
+        /// </summary>
         public Filter Filter
         {
             get { return _filter; }
             set { _filter = value; }
         }
+
+        /// <summary>
+        /// Initializes a new instance of the FilterAlreadyExistException class with its message string 
+        /// set to a system-supplied message
+        /// </summary>
+        public FilterAlreadyExistException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the FilterAlreadyExistException class with its message string 
+        /// set to message that is given by <see cref="ErrorMessage.FILTER_ALREADY_EXISTS_EXCEPTION">
+        /// ErrorMessage.FILTER_ALREADY_EXISTS_EXCEPTION</see> string value and the filter property to the 
+        /// filter that is passed as parameter
+        /// </summary>
+        /// <param name="filter"></param>
         public FilterAlreadyExistException(Filter filter)
+            : base(ErrorMessage.FILTER_ALREADY_EXISTS_EXCEPTION)
         {
             _filter = filter;
         }
