8fbb7c22f79dd9752bebc3092478dda9faf4aa2c
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-07 09:45:25 +0000

    added comment to CancelSyncNotification.cs

---  big5sync/Syncless/Syncless/Notification/CancelSyncNotification.cs
+++  big5sync/Syncless/Syncless/Notification/CancelSyncNotification.cs
@@ -5,21 +5,36 @@ using System.Text;
 
 namespace Syncless.Notification.UINotification
 {
+    /// <summary>
+    /// CancelSyncNotification class encloses properties for a notification for cancelling a sync request.
+    /// </summary>
     public class CancelSyncNotification : AbstractNotification
     {
         private readonly string _tagName;
         private readonly bool _isCancellable;
 
+        /// <summary>
+        /// Gets the tag name
+        /// </summary>
         public string TagName
         {
             get { return _tagName; }
         }
 
+        /// <summary>
+        /// Gets the boolean value that represents whether a sync request is cancellable
+        /// </summary>
         public bool IsCancellable
         {
             get { return _isCancellable; }
         }
 
+        /// <summary>
+        /// Creates a new CancelSyncNotification object
+        /// </summary>
+        /// <param name="tagName">The tag name of the sync request</param>
+        /// <param name="isCancellable">The boolean value that represents whether a sync request
+        /// is cancellable</param>
         public CancelSyncNotification(string tagName, bool isCancellable)
             : base("Cancel Sync Notification", Notification.NotificationCode.CancelSyncNotification)
         {
