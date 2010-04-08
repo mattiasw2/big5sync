ed5684a83e7c307f553a844b7beea647f9155e8d
gohkhoonhiang <gohkhoonhiang@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-08 09:25:23 +0000

    added comment to CorruptedTaggingXMLException.cs

--- trunk/ big5sync/Syncless/Syncless/Tagging/Exceptions/CorruptedTaggingXMLException.cs
+++ trunk/ big5sync/Syncless/Syncless/Tagging/Exceptions/CorruptedTaggingXMLException.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace Syncless.Tagging.Exceptions
 {
+    /// <summary>
+    /// The exception that is thrown when a tagging.xml file consists of invalid Xml tags.
+    /// </summary>
     public class CorruptedTaggingXMLException : Exception
     {
     }
