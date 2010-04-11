fe975bb660cad317deca14c42ba62602ee72b5bf
sohyuanchin <sohyuanchin@gmail.com>
2010-04-11 07:05:18 +0000

    Added Update Checker project

---
+++  big5sync/Syncless/SynclessUpdateChecker/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SynclessUpdateChecker
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(12, 12);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(244, 23);
+            this.buttonCheck.TabIndex = 0;
+            this.buttonCheck.Text = "Check For Updates";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 48);
+            this.Controls.Add(this.buttonCheck);
+            this.Name = "FormMain";
+            this.Text = "Syncless Update Checker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCheck;
+    }
+}
+
---
+++  big5sync/Syncless/SynclessUpdateChecker/UpdateChecker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+// Source: http://themech.net/2008/05/adding-check-for-update-option-in-csharp/
+namespace SynclessUpdateChecker
+{
+    public class UpdateChecker
+    {
+        public readonly string XmlUrl;
+        public readonly string NodeRootElement;
+        public const string NodeLatestVersion = "version";
+        public const string NodeUrl = "url";
+
+        private string _updateUrl;
+
+        public UpdateChecker()
+        {
+            XmlUrl = "http://big5sync.googlecode.com/files/LatestVersion.xml";
+            NodeRootElement = "syncless";
+        }
+
+        public UpdateChecker(string xmlUrl, string rootElement)
+        {
+            XmlUrl = xmlUrl;
+            NodeRootElement = rootElement;
+        }
+
+        /// <summary>
+        /// Retrieves and compares the version stored on the server against the current running version.
+        /// </summary>
+        /// <returns>1 if new updates are found, 0 if no newer version is found. -1 is thrown if there is an error.</returns>
+        public int GetNewVersion()
+        {
+            WebRequest request = WebRequest.Create(XmlUrl);
+            request.Timeout = 3000;
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+
+            Version newVersion = null;
+            XmlTextReader reader = null;
+
+            try
+            {
+                reader = new XmlTextReader(responseStream);
+                reader.MoveToContent();
+                string elementName = string.Empty;
+
+                if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == NodeRootElement))
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.NodeType == XmlNodeType.Element)
+                            elementName = reader.Name;
+                        else
+                        {
+                            if ((reader.NodeType == XmlNodeType.Text) && (reader.HasValue))
+                            {
+                                switch (elementName)
+                                {
+                                    case NodeLatestVersion:
+                                        newVersion = new Version(reader.Value);
+                                        break;
+                                    case NodeUrl:
+                                        _updateUrl = reader.Value;
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (newVersion == null || string.IsNullOrEmpty(_updateUrl))
+                    return -1;
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+
+            Version curVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            if (curVersion.CompareTo(newVersion) < 0)
+                return 1; // Newer version found
+            else
+                return 0; // Unable to find any updates
+        }
+
+        public string GetUpdateUrl
+        {
+            get { return _updateUrl; }
+        }
+
+    }
+}
