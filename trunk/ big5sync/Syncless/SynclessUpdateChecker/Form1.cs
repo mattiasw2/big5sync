6399af9933ee4c8a0282f5dccf9f55ae7397b8f6
sohyuanchin@gmail.com <sohyuanchin@gmail.com@e0f1b43a-ffa4-11de-9d47-03d50c098576>
2010-04-11 07:05:18 +0000

    Added Update Checker project

---
+++ trunk/ big5sync/Syncless/SynclessUpdateChecker/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SynclessUpdateChecker
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            UpdateChecker updateChecker = new UpdateChecker();
+            int newUpdate = updateChecker.GetNewVersion();
+
+            switch (newUpdate)
+            {
+                case 0:
+                    MessageBox.Show("New Update!" + "\n" + updateChecker.GetUpdateUrl);
+                    break;
+                case 1:
+                    MessageBox.Show("You Are Currently Running The Latest Version!");
+                    break;
+                case -1:
+                    MessageBox.Show("Error!");
+                    break;
+                default:
+                    Debug.Fail("Invalid value!");
+                    break;
+            }                
+        }
+    }
+}
